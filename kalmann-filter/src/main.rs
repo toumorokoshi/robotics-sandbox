9f04bf381412901e66e552c72831a7585d9e6528
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2026-05-10 20:59:17 -0700

    refactor: switch to ndarray for standard matrix abstraction
    The rationale to switch is to use the more standard abstraction as recommended by the rust book.

--- kalmann-filter/src/main.rs
+++ kalmann-filter/src/main.rs
@@ -1,33 +1,30 @@
-use matrixmultiply::dgemm;
+use ndarray::Array2;
+// A basic implementation for a Kalmann Filter.
+// - A kalmann filter predicts the next step of a set of variables, via a prediction matrix.
+// - If there is some external factor, a control matrix can also be added.
+// x_k = F_{k} * x_{k-1} + B_k * u_k
+// P_k = F_{k} * P_{k-1} * F^T_{k} + Q_{k}
+//
+// Where:
+// x is the state vector
+// F is the prediction matrix, predicting the next state from a model.
+// B is the control matrix, representing external contributors like a vehicle brake.
+// u is the control vector
+// P is the covariance matrix, representing the covariance of each variable being predicted.
+// Q is the process noise covariance matrix, representing the amount of innate variance in the model.
 
+// A prediction matrix predicts the next state
+// for the variables the kalmann filter is predicting.
 fn get_greeting() -> String {
     String::from("Hello, World!")
 }
 
 fn run_matrix_multiply_example() -> Vec<f64> {
-    let a = [1., 2., 3., 4.];
-    let b = [5., 6., 7., 8.];
-    let mut c = [0.; 4];
-
-    unsafe {
-        dgemm(
-            2,
-            2,
-            2,
-            1.0,
-            a.as_ptr(),
-            2,
-            1,
-            b.as_ptr(),
-            2,
-            1,
-            0.0,
-            c.as_mut_ptr(),
-            2,
-            1,
-        );
-    }
-    c.to_vec()
+    let a = Array2::from_shape_vec((2, 2), vec![1., 2., 3., 4.]).unwrap();
+    let b = Array2::from_shape_vec((2, 2), vec![5., 6., 7., 8.]).unwrap();
+    let c = a.dot(&b);
+    let (vec, _) = c.into_raw_vec_and_offset();
+    vec
 }
 
 fn main() {
